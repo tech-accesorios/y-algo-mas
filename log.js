97632388fced3a9f6a2cd740434cd147e6ad74f7
GiselayPablo <ggiselayppablo@hotmail.com>
2023-03-09 14:38:09 -0300

    pensando en "js"

--- log.js
+++ log.js
@@ -1,19 +1,18 @@
-// $contenedor= document.getElementsByClassName('contenedor'),
 const $buscador= document.getElementById('buscador'),
-$cardTitle= document.getElementById('card-title')
-
-let titulo= document.write($cardTitle.textContent)
-
-console.log(titulo)
+$cardTitle= document.querySelectorAll('.card-title')
+// $contenedor= document.querySelector('.contenedor')
 
 
 
 
 
 $buscador.addEventListener('keypress',()=>{
-  valorBuscador= $buscador.value
-
+  valorBuscador= $buscador.value.toUpperCase()
   let palabras= valorBuscador.split(' ')
-  
   console.log(palabras)
+})
+
+$cardTitle.forEach((prod)=>{
+  producto= prod.innerHTML.toUpperCase()
+  console.log(producto)
 })
