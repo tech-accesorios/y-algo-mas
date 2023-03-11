6eab6f7e68be1b3803f361ef15fa3ee4b9615e69
GiselayPablo <ggiselayppablo@hotmail.com>
2023-03-10 21:32:26 -0300

    arreglos nav

--- log.js
+++ log.js
@@ -2,17 +2,23 @@ const $buscador= document.getElementById('buscador'),
 $cardTitle= document.querySelectorAll('.card-title')
 // $contenedor= document.querySelector('.contenedor')
 
+let productos=[]
+$cardTitle.forEach((prod, indice)=>{
+  producto= prod.innerHTML.toUpperCase()
+   productos[indice]= producto
+})
+console.log(productos)
 
-
-
-
+var indices = [];
 $buscador.addEventListener('keypress',()=>{
   valorBuscador= $buscador.value.toUpperCase()
   let palabras= valorBuscador.split(' ')
-  console.log(palabras)
-})
 
-$cardTitle.forEach((prod)=>{
-  producto= prod.innerHTML.toUpperCase()
-  console.log(producto)
+let idx = productos.indexOf(palabras);
+while (idx != -1) {
+  indices.push(idx);
+  idx = array.indexOf(palabras, idx + 1);
+}
+console.log(indices);
+  
 })
