c84d2fd16c53e60e4c9990503ebac012095fdf34
GiselayPablo <ggiselayppablo@hotmail.com>
2023-04-01 11:39:43 -0300

    ...

--- log.js
+++ log.js
@@ -20,7 +20,7 @@ let ultimoScroll= scrollY
 window.addEventListener('scroll',()=>{
   let scrollActual= scrollY
   if(ultimoScroll> scrollActual){
-    btnNav.style.top='calc(100vh - 48px)'
+    btnNav.style.top='calc(100vh - 88px)'
     navcont.style.top='0'
   }
   else{
